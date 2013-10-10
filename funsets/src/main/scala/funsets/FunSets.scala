985f02e3dcb3a14061f08bfbad3ad1dc3f30ab8e
Sergey Skovorodkin <ls.sergey@gmail.com>
2013-10-10 21:18:16 +0400

    [funsets] Fix exists

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -65,21 +65,14 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (s(a) && p(a)) true
-      else if (a == bound) false
-      else iter(a + 1)
-    }
-    iter(-bound)
-  }
+  def exists(s: Set, p: Int => Boolean): Boolean =
+    !forall(s, (x: Int) => !p(x))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = (x: Int) => {
+  def map(s: Set, f: Int => Int): Set = (x: Int) =>
     exists(s, (y: Int) => f(y) == x)
-  }
 
   /**
    * Displays the contents of a set
